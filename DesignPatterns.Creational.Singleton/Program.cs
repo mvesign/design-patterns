41a82c049fc0ff7943576b6b7535ced5dc2924b2
Maikel Vreugde <maikel.vreugde@cm.nl>
2021-11-10 15:07:07 +0100

    Add singleton pattern

---
+++ DesignPatterns.Creational.Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(Sun.Instance);
+            Console.WriteLine(Sun.Instance);
+            Console.WriteLine(Sun.Instance);
+            Console.WriteLine(Sun.Instance);
+            Console.WriteLine(Sun.Instance);
+        }
+    }
+}
---
+++ DesignPatterns.Creational.Singleton/Sun.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    /// <summary>
+    /// The one and only sun.
+    /// </summary>
+    public class Sun
+    {
+        /// <summary>
+        /// Private constructor to force the single instance
+        /// </summary>
+        private Sun()
+        {
+            Identifier = new Random().Next();
+        }
+
+        /// <summary>
+        /// Get the one and only instance of the sun.
+        /// </summary>
+        public static Sun Instance { get; } = new();
+
+        /// <summary>
+        /// Identifier of the sun.
+        /// </summary>
+        public int Identifier { get; }
+
+        /// <inheritdoc />
+        public override string ToString() => $"Sun #{Identifier}";
+    }
+}
